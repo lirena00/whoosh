bc943bbd76712bcb9620b64d71a382428867a390
lirena <sakshamkushwaha12776@gmail.com>
2025-06-07 16:51:28 +0530

    fix: baseUrl auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://whoosh-three.vercel.app"
-      : "http://localhost:3000",
 });
 
 export const { signIn, signOut, useSession } = authClient;
