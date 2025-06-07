34f01c41d620306ad77181f2e246b2fb93e84dd4
lirena <sakshamkushwaha12776@gmail.com>
2025-06-07 13:57:14 +0530

    fix: baseurl authentication

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,10 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: `${process.env.VERCEL_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000"}`,
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://whoosh-three.vercel.app"
+      : "http://localhost:3000",
 });
 
 export const { signIn, signOut, useSession } = authClient;
